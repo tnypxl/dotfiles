efbd0f371714851314e2125fa8c1dc4f5c455488
Arik Jones <github@tnypxl.com>
2024-06-28 11:36:28 -0500

    Added cody ai completions/chat. Font size changes for alacritty.

---
+++ .config/lvim/config.lua
@@ -0,0 +1,27 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/iXb9B4Ny
+
+
+lvim.plugins = {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavor = "mocha"
+    }
+  },
+  {
+    "sourcegraph/sg.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("sg").setup()
+    end,
+  }
+}
+
+vim.opt.tabstop = 4
+
+table.insert(lvim.builtin.cmp.sources, { name ="cody" })
