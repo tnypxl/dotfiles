500d57bdc7a03fe082056a204c0a93a8ce9c01de
Arik Jones <arikj@bucklehq.com>
2025-03-24 13:23:15 -0500

    neovim: 4 space indentations

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,3 +4,11 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+local o = vim.o
+
+-- Add 4 spaces for indentation
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+o.expandtab = true
