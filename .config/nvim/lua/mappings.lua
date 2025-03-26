f29bfc6db11d651a36d05728060daa3ee6ec6d7e
Arik Jones <arikj@bucklehq.com>
2025-03-26 18:52:37 -0500

    neovim: update kep maps to toggle nvim-tree

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -9,8 +9,10 @@ map("i", "jk", "<ESC>")
 map({ "i", "v", "n" }, "<C-s>", "<cmd>w<cr>")
 map({ "n", "v" }, "<Leader>q", "<cmd>qa<cr>")
 
--- CodeCompanion
+-- NvimTree
+map("n", "<Leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
+-- CodeCompanion
 map({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
 map({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
 map("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,14 +1,9 @@
 require "nvchad.options"
 
--- add yours here!
-
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
-
 local o = vim.o
 
--- Add 4 spaces for indentation
 o.shiftwidth = 4
 o.tabstop = 4
 o.softtabstop = 4
 o.expandtab = true
+o.scrolloff = 4
