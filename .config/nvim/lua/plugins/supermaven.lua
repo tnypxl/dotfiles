125d6292b8789fa9a5486e2914a894a5c0db3389
Arik Jones <arikj@bucklehq.com>
2025-03-26 18:50:42 -0500

    neovim: update supermaven configs

--- .config/nvim/lua/plugins/supermaven.lua
+++ .config/nvim/lua/plugins/supermaven.lua
@@ -3,7 +3,11 @@ return {
     "supermaven-inc/supermaven-nvim",
     lazy = false,
     config = function()
-      require("supermaven-nvim").setup {}
+      require("supermaven-nvim").setup {
+        -- your configuration comes here
+        disable_keymaps = true,
+        disable_inline_completion = true,
+      }
     end,
   },
 }
