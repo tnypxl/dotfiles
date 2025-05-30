ce3ab168ef80de1e4a47476160878a32e13034c2
Arik Jones <arikj@bucklehq.com>
2025-05-30 17:01:50 -0500

    neovim: formatting

--- .config/nvim/lua/custom/configs/avante.lua
+++ .config/nvim/lua/custom/configs/avante.lua
@@ -10,16 +10,19 @@ M.setup = function()
         model = "google/gemini-2.5-pro-preview",
       },
     },
+
     system_prompt = function()
       local hub = require("mcphub").get_hub_instance()
       return hub:get_active_servers_prompt()
     end,
+
     -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
     custom_tools = function()
       return {
         require("mcphub.extensions.avante").mcp_tool(),
       }
     end,
+
     disabled_tools = {
       "list_files",
       "search_files",
@@ -32,9 +35,11 @@ M.setup = function()
       "delete_dir",
       "bash",
     },
+
     behavior = {
       enable_token_counting = false,
     },
+
     windows = {
       sidebar_header = {
         enabled = true,
