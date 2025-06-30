7214a1d80aa5668ec3d6f0f17b7eb736b9673670
Arik Jones <arikj@bucklehq.com>
2025-06-30 09:49:10 -0500

    neovim: remove current nvim install

--- .config/nvim/after/ftplugin/javascript.lua
+++
@@ -1,9 +0,0 @@
-vim.opt_local.expandtab = true
-vim.opt_local.tabstop = 2
-vim.opt_local.shiftwidth = 2
-vim.opt_local.softtabstop = 2
--- vim.opt_local.textwidth = 80
--- vim.opt_local.colorcolumn = "80"
-
--- JS-specific keymaps
-vim.keymap.set("n", "<leader>nr", "<cmd>!node %<CR>", { buffer = true, desc = "Run current JS file" })
