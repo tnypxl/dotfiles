c9d538349141942960e5d7a611256560943f4dc4
Arik Jones <arikj@bucklehq.com>
2025-07-14 09:34:09 -0500

    neovim: some neovim autocommands, attempt to support shift+enter new lines in neovim terminal

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -7,3 +7,13 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 --
+
+-- Markdown soft wrap at 80 columns
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.textwidth = 80
+    vim.opt_local.linebreak = true
+  end,
+})
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Insert mode keybindings
+vim.keymap.set({ "i", "v" }, "jk", "<ESC>", { noremap = true, silent = true, desc = "Exit insert mode" })
+
+-- Terminal mode keybindings
+vim.keymap.set("t", "<S-CR>", "<C-J>", { noremap = true, silent = true, desc = "Terminal: Literal Newline" })
