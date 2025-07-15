be1e05dab6deab71c6e433a3fa90f193f289d882
Arik Jones <arikj@bucklehq.com>
2025-07-15 10:51:46 -0500

    neovim: some minor changes

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -1,6 +1,14 @@
 return {
   {
     "rebelot/kanagawa.nvim",
+    init = function()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "kanagawa",
+        callback = function()
+          vim.api.nvim_set_hl(0, "StatusLine", { link = "lualine_c_normal" })
+        end,
+      })
+    end,
     opts = {
       colors = {
         theme = {
@@ -20,4 +28,3 @@ return {
     },
   },
 }
-
