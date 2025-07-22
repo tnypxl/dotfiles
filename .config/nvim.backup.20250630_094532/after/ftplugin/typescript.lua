dec08881977e45f25f2a05d1e74952e3fb36432a
Arik Jones <arikj@bucklehq.com>
2025-07-22 12:26:13 -0500

    removing cruft

--- .config/nvim.backup.20250630_094532/after/ftplugin/typescript.lua
+++
@@ -1,5 +0,0 @@
--- TypeScript can inherit from JavaScript settings
-vim.cmd "runtime! after/ftplugin/javascript.lua"
-
--- Add any TypeScript-specific overrides
-vim.keymap.set("n", "<leader>tc", "<cmd>TypescriptCompile<CR>", { buffer = true, desc = "Compile TypeScript" })
--- .config/nvim.backup.20250630_094532/lua/plugins/cmp.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "hrsh7th/nvim-cmp",
-  opts = function(_, opts)
-    return require("custom.configs.cmp").setup(opts)
-  end,
-}
