8eea41a1f66e7e9e521521a354c6fd4f50fa74c6
Arik Jones <tnypxl@hey.com>
2025-08-08 01:00:03 -0500

    neovim: fix lualine blank spots ... again?

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -3,7 +3,6 @@ return {
     "rebelot/kanagawa.nvim",
     init = function()
       vim.api.nvim_create_autocmd("ColorScheme", {
-        pattern = "kanagawa",
         callback = function()
           vim.api.nvim_set_hl(0, "StatusLine", { link = "lualine_c_normal" })
         end,
